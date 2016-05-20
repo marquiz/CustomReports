50e5d5253d413937ac34f8ac7ee1d55d105146a3
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2016-05-20 16:11:56 +0300

    Add wireframe of build_perf app
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

---
+++ build_perf/views.py
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2016, Intel Corporation.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+"""Views for build_perf app"""
+from django.http import HttpResponse
+
+
+def index(request):
+    """Dummy index page"""
+    return HttpResponse("Build perf test results coming soon...")
