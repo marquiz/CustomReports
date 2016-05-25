84a4ea475139d884957a7b8b001a27b756abab3f
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2016-05-25 17:43:55 +0300

    Add build_perf as a new django app
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- customreports/urls.py
+++ customreports/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^build_perf/', include('build_perf.urls', namespace="build_perf")),
     url(r'', include('charts.urls', namespace="charts"))
 ]
